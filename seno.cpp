0247da06368f75f577820dec8647d180c7a4e6a7
Camila Diaz <marcdiazsan@unal.edu.co>
2018-03-01 09:47:39 -0500

    Segunda version

--- seno.cpp
+++ seno.cpp
@@ -1,26 +1,36 @@
 #include<iostream>
 #include<cmath>
-const double x=0.2; //rad;
+const double x=(M_PI)/2; //rad;
 double seno (int N);
 double termino (double m);
 int main()
-{
-  
+{std::cout.precision(16);
+  int num,ii;
+  double b,a,error;
+  num=20;
+  for(ii=1;ii<=num;ii++)
+    {
+      b=seno(ii);
+      a=std::sin(x);
+      error=((std::abs(a-b))/(std::abs(a)))*100;
+      std::cout<<b<<"  "<<error<<std::endl;
+    }
 }
-double termino (int m)
+
+double termino (double m)
 {double a;
-  if (m==1)
+  if(m==1.0)
     {a=x;}
   else
-    {a=termino(m-1)*(((-1)*(std::pow(x,2)))/((2*m)+1));
+    {a=(termino(m-1)*(-1)*(std::pow(x,2)))/(((2*m)-1)*((2*m)-2));
     }
   return a;
 }
-
+ 
 double seno (int N)
 {double sum, ter;
   sum=0;
-  for(int i=0; i<=N; i++)
+  for(double i=1.0; i<=N; i++)
     {
       ter=termino(i);
       sum+=ter;
